42de26258a787e177b35c62f4302de21330142f3
ryandeussing <ryan@ryandeussing.com>
2017-02-20 17:38:22 -0500

    dev bootstrap with jekyllized and neat

---
+++ gulp/tasks/images.js
@@ -0,0 +1,32 @@
+'use strict';
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const size = require('gulp-size');
+
+// 'gulp images' -- optimizes and caches your images
+gulp.task('images', () =>
+  gulp.src('src/assets/images/**/*')
+    .pipe(cache(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng(),
+      imagemin.svgo({plugins: [{cleanupIDs: false}]})
+    ])))
+    .pipe(gulp.dest('.tmp/assets/images'))
+    .pipe(gulp.dest('src/assets/images')) // for cloud cannon
+    .pipe(size({title: 'images'}))
+);
+
+// 'gulp uploads' -- copies uploads directory
+gulp.task('uploads', () =>
+  gulp.src('src/uploads/**/*')
+    .pipe(cache(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng(),
+      imagemin.svgo({plugins: [{cleanupIDs: false}]})
+    ])))
+    .pipe(gulp.dest('.tmp/uploads'))
+    .pipe(gulp.dest('src/uploads')) // for cloud cannon
+);
---
+++ gulp/tasks/uploading.js
@@ -0,0 +1,2 @@
+'use strict';
+// File empty but generated because of how Yeoman scaffolds files
