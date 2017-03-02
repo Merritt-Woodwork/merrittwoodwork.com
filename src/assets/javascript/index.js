1bf074f5e89b7ff4b52c852e855539f0b224d483
ryandeussing <ryan@ryandeussing.com>
2017-03-02 13:24:31 -0500

    burger

--- src/assets/javascript/index.js
+++ src/assets/javascript/index.js
@@ -10927,6 +10927,8 @@ window.Modernizr = (function( window, document, undefined ) {
   (function() {
     var triggerBttn = document.getElementById( 'trigger-overlay' ),
       overlay = document.querySelector( 'div.overlay' ),
+      body = document.querySelector( 'body' ),
+      burger = document.querySelector(".c-hamburger");
       closeBttn = overlay.querySelector( 'button.overlay-close' );
       transEndEventNames = {
         'WebkitTransition': 'webkitTransitionEnd',
@@ -10941,7 +10943,9 @@ window.Modernizr = (function( window, document, undefined ) {
     function toggleOverlay() {
       if( classie.has( overlay, 'open' ) ) {
         classie.remove( overlay, 'open' );
+        classie.remove( burger, 'is-active' );
         classie.add( overlay, 'close' );
+        classie.remove( body, 'open' );
         var onEndTransitionFn = function( ev ) {
           if( support.transitions ) {
             if( ev.propertyName !== 'visibility' ) return;
@@ -10958,12 +10962,26 @@ window.Modernizr = (function( window, document, undefined ) {
       }
       else if( !classie.has( overlay, 'close' ) ) {
         classie.add( overlay, 'open' );
+        classie.add( body, 'open' );
+        classie.add( burger, 'is-active' );
       }
     }
 
     triggerBttn.addEventListener( 'click', toggleOverlay );
     closeBttn.addEventListener( 'click', toggleOverlay );
   })();
+
+  // hamburger
+  var toggle = document.querySelector(".c-hamburger");
+
+    toggleHandler(toggle);
+
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      });
+    }
 
 })();
 //# sourceMappingURL=index.js.map
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -3,6 +3,8 @@
   (function() {
     var triggerBttn = document.getElementById( 'trigger-overlay' ),
       overlay = document.querySelector( 'div.overlay' ),
+      body = document.querySelector( 'body' ),
+      burger = document.querySelector(".c-hamburger");
       closeBttn = overlay.querySelector( 'button.overlay-close' );
       transEndEventNames = {
         'WebkitTransition': 'webkitTransitionEnd',
@@ -17,7 +19,9 @@
     function toggleOverlay() {
       if( classie.has( overlay, 'open' ) ) {
         classie.remove( overlay, 'open' );
+        classie.remove( burger, 'is-active' );
         classie.add( overlay, 'close' );
+        classie.remove( body, 'open' );
         var onEndTransitionFn = function( ev ) {
           if( support.transitions ) {
             if( ev.propertyName !== 'visibility' ) return;
@@ -34,11 +38,25 @@
       }
       else if( !classie.has( overlay, 'close' ) ) {
         classie.add( overlay, 'open' );
+        classie.add( body, 'open' );
+        classie.add( burger, 'is-active' );
       }
     }
 
     triggerBttn.addEventListener( 'click', toggleOverlay );
     closeBttn.addEventListener( 'click', toggleOverlay );
   })();
+
+  // hamburger
+  var toggle = document.querySelector(".c-hamburger");
+
+    toggleHandler(toggle);
+
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      });
+    }
 
 })();
